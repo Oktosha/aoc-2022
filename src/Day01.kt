27b615dda60ff18782819e92100be92dfa88520e
Daria Kolodzey <kolodaria@gmail.com>
2022-12-01 22:04:09 +0100

    Add solution to Day1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,42 @@
+import kotlin.math.max
+import java.util.PriorityQueue
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var answer = 0
+        var current = 0
+        for (line in input) {
+            if (line.trim() == "") {
+                answer = max(current, answer)
+                current = 0
+            } else {
+                current += line.toInt()
+            }
+        }
+        answer = max(current, answer)
+        return answer
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val highestValues = PriorityQueue<Int>()
+        var current = 0
+        for (line in input) {
+            if (line.trim() == "") {
+                highestValues.add(current)
+                if (highestValues.size > 3) {
+                    highestValues.remove()
+                }
+                current = 0
+            } else {
+                current += line.toInt()
+            }
+        }
+        highestValues.add(current)
+        if (highestValues.size > 3) {
+            highestValues.remove()
+        }
+        return highestValues.fold(0) { x, y -> x + y }
     }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     println(part1(input))
