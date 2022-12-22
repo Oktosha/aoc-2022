b915eb35b51bede21f6eecae277a9bd194ae4290
Daria Kolodzey <kolodaria@gmail.com>
2022-12-22 20:21:25 +0400

    Move solution inside function in Day12 to prevent namespace pollution

--- src/Day12.kt
+++ src/Day12.kt
@@ -1,71 +1,69 @@
-class Position(val h: Int, val w: Int)
 typealias Grid = MutableList<MutableList<Int>>
 
-// For some reason this function is falsely marked unused by the inspection
-// but when I suppress the warning, the inspection says that suppression is redundant
-operator fun Grid.get(pos: Position): Int {
-    return this[pos.h][pos.w]
-}
+fun main() {
+    class Position(val h: Int, val w: Int)
 
-operator fun Grid.set(pos: Position, value: Int) {
-    this[pos.h][pos.w] = value
-}
-
-// Same false positive for unused
-operator fun List<String>.get(pos: Position): Char {
-    return this[pos.h][pos.w]
-}
-
-fun Grid.neigbours(pos: Position): List<Position> {
-    val delta = listOf(Position(0, 1), Position(1, 0), Position(0, -1), Position(-1, 0))
-    return delta.map { d -> Position(d.h + pos.h, d.w + pos.w) }
-        .filter { p -> p.h >= 0 && p.w >= 0 && p.h < this.size && p.w < this[0].size }
-}
-
-fun findLetterPosition(input: List<String>, letter: Char): Position {
-    val h = input.indexOfFirst { line -> line.contains(letter) }
-    assert(h != -1)
-    val w = input[h].indexOf(letter)
-    return Position(h, w)
-}
-
-fun elevation(c: Char): Int {
-    fun posInAlpnabet(x: Char): Int {
-        return x.code - 'a'.code
+    operator fun Grid.get(pos: Position): Int {
+        return this[pos.h][pos.w]
     }
-    assert((c in 'a'..'z') || c == 'S' || c == 'E')
-    if (c == 'S') {
-        return posInAlpnabet('a')
-    }
-    if (c == 'E') {
-        return posInAlpnabet('z')
-    }
-    return posInAlpnabet(c)
-}
 
-fun initBFSPart1(input: List<String>, grid: Grid): ArrayDeque<Position> {
-    val startPosition = findLetterPosition(input, 'S')
-    grid[startPosition] = 0
-    val queue = ArrayDeque(listOf(startPosition))
-    queue.addLast(startPosition)
-    return queue
-}
+    operator fun Grid.set(pos: Position, value: Int) {
+        this[pos.h][pos.w] = value
+    }
 
-fun initBFSPart2(input: List<String>, grid: Grid): ArrayDeque<Position> {
-    val queue = ArrayDeque<Position>()
-    for (h in input.indices) {
-        for (w in input[h].indices) {
-            val pos = Position(h, w)
-            if (elevation(input[pos]) == 0) {
-                grid[pos] = 0
-                queue.addLast(pos)
+    // Same false positive for unused
+    operator fun List<String>.get(pos: Position): Char {
+        return this[pos.h][pos.w]
+    }
+
+    fun Grid.neigbours(pos: Position): List<Position> {
+        val delta = listOf(Position(0, 1), Position(1, 0), Position(0, -1), Position(-1, 0))
+        return delta.map { d -> Position(d.h + pos.h, d.w + pos.w) }
+            .filter { p -> p.h >= 0 && p.w >= 0 && p.h < this.size && p.w < this[0].size }
+    }
+
+    fun findLetterPosition(input: List<String>, letter: Char): Position {
+        val h = input.indexOfFirst { line -> line.contains(letter) }
+        assert(h != -1)
+        val w = input[h].indexOf(letter)
+        return Position(h, w)
+    }
+
+    fun elevation(c: Char): Int {
+        fun posInAlpnabet(x: Char): Int {
+            return x.code - 'a'.code
+        }
+        assert((c in 'a'..'z') || c == 'S' || c == 'E')
+        if (c == 'S') {
+            return posInAlpnabet('a')
+        }
+        if (c == 'E') {
+            return posInAlpnabet('z')
+        }
+        return posInAlpnabet(c)
+    }
+
+    fun initBFSPart1(input: List<String>, grid: Grid): ArrayDeque<Position> {
+        val startPosition = findLetterPosition(input, 'S')
+        grid[startPosition] = 0
+        val queue = ArrayDeque(listOf(startPosition))
+        queue.addLast(startPosition)
+        return queue
+    }
+
+    fun initBFSPart2(input: List<String>, grid: Grid): ArrayDeque<Position> {
+        val queue = ArrayDeque<Position>()
+        for (h in input.indices) {
+            for (w in input[h].indices) {
+                val pos = Position(h, w)
+                if (elevation(input[pos]) == 0) {
+                    grid[pos] = 0
+                    queue.addLast(pos)
+                }
             }
         }
+        return queue
     }
-    return queue
-}
-
-fun main() {
 
     fun solve(input: List<String>, initBFS: (List<String>, Grid) -> ArrayDeque<Position>): Int {
         val width = input[0].length
