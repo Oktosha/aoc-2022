9954e3d19002b72886b8fc819ad78bc6c88fb570
Daria Kolodzey <kolodaria@gmail.com>
2022-12-24 02:25:16 +0400

    Day 23 part2

--- src/day23/Day23.kt
+++ src/day23/Day23.kt
@@ -45,7 +45,12 @@ data class Position(val row: Int, val column: Int) {
     operator fun plus(other: Position): Position {
         return Position(row + other.row, column + other.column)
     }
-
+    operator fun minus(other: Position): Position {
+        return Position(row - other.row, column - other.column)
+    }
+    fun area(): Int {
+        return row * column
+    }
     fun neigbours(): List<Position> {
         return Direction.values().map { x -> x.vec + this }
     }
@@ -58,11 +63,19 @@ data class Position(val row: Int, val column: Int) {
 typealias State = Set<Position>
 
 fun State.part1Answer(): Int {
-    val startRow = this.minOf { p -> p.row }
-    val endRow = this.maxOf { p -> p.row }
-    val startColumn = this.minOf { p -> p.column }
-    val endColumn = this.maxOf { p -> p.column }
-    return (endRow - startRow + 1) * (endColumn - startColumn + 1) - this.size
+    return (bottomRight() - topLeft() + Position(1, 1)).area() - this.size
+}
+
+fun State.topLeft(): Position {
+    return Position(this.minOf { x -> x.row }, this.minOf { x -> x.column })
+}
+
+fun State.bottomRight(): Position {
+    return Position(this.maxOf { x -> x.row }, this.maxOf { x -> x.column })
+}
+
+fun State.normalized(): State {
+    return this.map { x -> x - this.topLeft() }.toSet()
 }
 
 fun State.proposition(elf: Position, considerationSequence: List<Direction>): Position? {
@@ -77,7 +90,7 @@ fun State.proposition(elf: Position, considerationSequence: List<Direction>): Po
     return null
 }
 
-fun State.nextWith(considerationSequence: List<Direction>): State {
+fun State.nextWith(considerationSequence: List<Direction>): State? {
     val movingElves = this
         .asSequence()
         .map { x -> x to proposition(x, considerationSequence) }
@@ -86,6 +99,10 @@ fun State.nextWith(considerationSequence: List<Direction>): State {
         .filter { x -> x.value.size == 1 }
         .map { x -> x.value[0]}
         .toList().toMap()
+    // println("Number of moving elves: ${movingElves.size}")
+    if (movingElves.isEmpty()) {
+        return null
+    }
     val nextState = mutableSetOf<Position>()
     for (elf in this) {
         if (elf in movingElves) {
@@ -112,9 +129,22 @@ fun readState(input: List<String>): State {
 fun simulate(startState: State, startStep: Int, numberOfSteps: Int): State {
     var state = startState
     for (step in startStep until startStep + numberOfSteps) {
-        state = state.nextWith(Direction.considerationSequence(step))
+        // print("step $step: ")
+        state = state.nextWith(Direction.considerationSequence(step)) ?: state
     }
     return state
+}
+
+fun simulateUntilEnd(startState: State): Int {
+    var current = startState
+    var stepOfNextState = 1
+    var next = current.nextWith(Direction.considerationSequence(stepOfNextState))
+    while (next != null) {
+        current = next
+        stepOfNextState += 1
+        next = current.nextWith(Direction.considerationSequence(stepOfNextState))
+    }
+    return stepOfNextState
 }
 
 fun prettyPrintState(state: State) {
@@ -161,13 +191,50 @@ fun parseTestData(input: List<String>): List<Pair<Int, State>> {
     return testData
 }
 
+fun testSimulationToEnd(startState: State, steps: Int) {
+    val simulatedSteps = simulateUntilEnd(startState)
+    if (simulatedSteps != steps) {
+        throw Exception("Simulation took $simulatedSteps instead of $steps")
+    }
+}
+
+fun countStepsBetween(startState: State, endState: State, threshold: Int): Int {
+    var state = startState
+    var step = 1
+    while (step < threshold && state.normalized() != endState.normalized()) {
+        state = state.nextWith(Direction.considerationSequence(step))!!
+        step += 1
+    }
+    return step - 1
+}
+
+fun testCountSteps(startState: State, endState: State, expectedSteps: Int, threshold: Int) {
+    val steps = countStepsBetween(startState, endState, threshold)
+    if (steps != expectedSteps) {
+        throw Exception("Steps is $steps instead of $expectedSteps")
+    }
+}
+
 fun main() {
     val smallTestData = parseTestData(readInput("Day23-test"))
-    testSimulation(smallTestData)
     val bigTestData = parseTestData(readInput("Day23-test2"))
+    val extraTestState = readState(readInput("Day23-extratest"))
+    // println("test small simulation")
+    testSimulation(smallTestData)
+    // println("test big simulation")
     testSimulation(bigTestData)
+    // println("Simulationg small test data")
+    simulate(smallTestData[0].second, 1, 10)
+    // println("Simulating big test Data")
+    simulate(bigTestData[0].second, 1, 25)
+    testCountSteps(smallTestData[0].second, smallTestData[3].second, 3, 100)
+    testCountSteps(bigTestData[0].second, extraTestState, 19, 100)
+    testSimulationToEnd(smallTestData[0].second, 4)
+    testSimulationToEnd(bigTestData[0].second, 20)
     println("Day 23")
     val input = readInput("Day23")
     val ans = simulate(readState(input), 1, 10).part1Answer()
     println("Answer part1: $ans")
+    val ans2 = simulateUntilEnd(readState(input))
+    println("Answer part2: $ans2")
 }
